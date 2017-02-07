85cdb726bcafa17ea91e9522b0368167ddbf75b2
Jesse McIntosh <jessemc98@hotmail.com>
2017-02-07 19:55:46 +0100

    added some initial todos to state

--- src/todoStore/initialState.js
+++ src/todoStore/initialState.js
@@ -1,3 +1,36 @@
 export default {
-  todos: []
+  todos: [
+    {
+      id: 1,
+      title: "Morning run",
+      time: "7:00",
+      checked: true
+    },
+    {
+      id: 2,
+      title: "Shower",
+      time: "8:00"
+    },
+    {
+      id: 3,
+      title: "Make Breakfast",
+      time: "9:00",
+      highlight: "blue"
+    },
+    {
+      id: 4,
+      title: "Get ready for work",
+      time: "10:00"
+    },
+    {
+      id: 5,
+      title: "Park on roundabout",
+      time: "12:00"
+    },
+    {
+      id: 6,
+      title: "Smash face on keyboard",
+      time: "14:00"
+    }
+  ]
 }
