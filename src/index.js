54aa2ba8f85bab679fd7c950477bc82562244abb
Jesse McIntosh <jessemc98@hotmail.com>
2017-02-21 08:13:23 +0100

    changed index.js to use a linkedList to synchronize the view with store.todos

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
-import log from './test'
 import './index.scss'
+import store from './todoStore/store'
+import createList from './listComponent/linkedList'
 
-log()
+// create a list linked to the store
+const list = createList(store, 'todos', {id: 'list'})
+
+// append list node to document
+document.getElementById('main').appendChild(list.node)
